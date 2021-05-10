f16a7ba9361371a42814a8ff58b0707c20a0a8f7
RuDomitori <RuDomitori@gmail.com>
2021-05-10 16:34:09 +0700

    Root folder

---
+++ Web/Controllers/FoldersController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Localization;
+using Web.Data;
+using Web.ViewModels;
+
+namespace Web.Controllers
+{
+    public class FoldersController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public FoldersController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        
+        public IActionResult Index()
+        {
+            var folders = _dbContext.Folders.Include(x=>x.Owner).Where(x => x.ParentFolderId == null).ToList();
+            var files = _dbContext.Files.Where(x => x.FolderId == null).ToList();
+            
+            return View(new FolderViewModel
+            {
+                Name = "Root",
+                Folders = folders,
+                Files = files,
+                Path = new List<(string, Guid)>()
+            });
+        }
+
+        private List<(string name, Guid id)> GetPath(Guid folderId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Web/ViewModels/FolderViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Web.Models;
+
+namespace Web.ViewModels
+{
+    public class FolderViewModel
+    {
+        public Guid? Id { get; set; }
+
+        public List<(string, Guid)> Path { get; set; }
+        
+        public string Name { get; set; }
+        
+        public ICollection<Folder> Folders { get; set; }
+        
+        public ICollection<File> Files { get; set; }
+    }
+}
