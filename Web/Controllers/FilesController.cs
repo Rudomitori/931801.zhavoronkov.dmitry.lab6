8208aad2e11cb5ff706044828eeba06a0789fe1e
RuDomitori <RuDomitori@gmail.com>
2021-05-16 14:16:28 +0700

    File deleting

--- Web/Controllers/FilesController.cs
+++ Web/Controllers/FilesController.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Linq;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Web.Data;
@@ -28,6 +30,29 @@ namespace Web.Controllers
                     return NotFound();
 
                 return File(file.Body,"application/octet-stream", file.Name);
+            }
+
+            return BadRequest();
+        }
+        
+        [HttpGet("[controller]/{id:guid}/Delete")]
+        [Authorize]
+        public IActionResult Delete(Guid id)
+        {
+            if (ModelState.IsValid)
+            {
+                var file = _dbContext.Files.FirstOrDefault(x => x.Id == id);
+                if (file is null)
+                    return NotFound();
+
+                var userId = new Guid(HttpContext.User.FindFirstValue("Id"));
+                if (file.OwnerId != userId && HttpContext.User.IsInRole("Admin"))
+                    return Unauthorized();
+
+                _dbContext.Remove(file);
+                _dbContext.SaveChanges();
+
+                return Redirect($"/Folders/{file.FolderId}");
             }
 
             return BadRequest();
